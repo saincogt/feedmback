cc45369539f4ae3d6e17a51c84562d92221ea510
saincogt <saincogt@gmail.com>
2020-07-09 22:08:42 +0930

    add google auth, logout and api current user

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,5 +8,29 @@ module.exports = (app) => {
 		})
 	);
 
-	app.get('/auth/google/callback', passport.authenticate('google'));
+	app.get(
+		'/auth/google/callback',
+		passport.authenticate('google'),
+		(err, req, res, next) => {
+			// custom error handler to catch any errors, such as TokenError
+			if (err.name === 'TokenError') {
+				res.redirect('/auth/google'); // redirect them back to the login page
+			} else {
+				// Handle other errors here
+			}
+		},
+		(req, res) => {
+			// On success, redirect back to '/'
+			res.redirect('/');
+		}
+	);
+
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	});
 };
