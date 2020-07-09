ef299e4e30b32594559ca3772499731719a8a90f
saincogt <saincogt@gmail.com>
2020-07-09 22:08:12 +0930

    setup mongoose and passport

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
 require('./services/passport');
+const keys = require('./config/keys');
 
 const PORT = process.env.PORT || 5000;
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
