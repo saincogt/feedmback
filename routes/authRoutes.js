657ff0aed00f9427d0dc321489c0e9fcd86f438d
saincogt <saincogt@gmail.com>
2020-07-11 15:52:40 +0930

    update auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,24 +10,16 @@ module.exports = (app) => {
 
 	app.get(
 		'/auth/google/callback',
-		passport.authenticate('google'),
-		(err, req, res, next) => {
-			// custom error handler to catch any errors, such as TokenError
-			if (err.name === 'TokenError') {
-				res.redirect('/auth/google'); // redirect them back to the login page
-			} else {
-				// Handle other errors here
-			}
-		},
-		(req, res) => {
-			// On success, redirect back to '/'
-			res.redirect('/');
-		}
+		passport.authenticate('google', {
+			successRedirect: '/survey',
+			failureRedirect: '/',
+			failureFlash: true
+		})
 	);
 
 	app.get('/api/logout', (req, res) => {
 		req.logout();
-		res.send(req.user);
+		res.redirect('/');
 	});
 
 	app.get('/api/current_user', (req, res) => {
