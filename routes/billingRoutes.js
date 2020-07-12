3527e0d2e2bb78ad5521633c1b4e46f2493ee4df
saincogt <saincogt@gmail.com>
2020-07-12 12:24:25 +0930

    add general require login middleware

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,17 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecret);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
-	app.post('/api/stripe', async (req, res) => {
-		const charge = await stripe.charges.create({
+	app.post('/api/stripe', requireLogin, async (req, res) => {
+		await stripe.charges.create({
 			amount: 500,
 			currency: 'AUD',
 			description: '$5 for 5 credits',
 			source: req.body.id
 		});
-		console.log(charge);
+		req.user.credits += 5;
+		const user = await req.user.save();
+		res.send(user);
 	});
 };
