b0c1cca6ab09cf67000c154f3f5e2172f3f064d2
saincogt <saincogt@gmail.com>
2020-07-12 12:24:09 +0930

    add prod config

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
-	cookieKey: process.env.COOKIE_KEY
+	cookieKey: process.env.COOKIE_KEY,
+	stripePK: process.env.STRIPE_PK,
+	stripeSecret: process.env.STRIPE_SECRET
 };
