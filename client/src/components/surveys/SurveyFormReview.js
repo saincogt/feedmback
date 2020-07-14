acac1bd85882c64433d9e129357f9d150005e42b
saincogt <saincogt@gmail.com>
2020-07-14 12:06:12 +0930

    withRouter to redirect to specific route

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import _ from 'lodash';
 import formFields from './formFields';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
 	const reviewFields = _.map(formFields, ({ name, label }) => {
 		return (
 			<div key={name}>
@@ -25,7 +26,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 			</button>
 			<button
 				className='green white-text btn-flat right'
-				onClick={() => submitSurvey(formValues)}
+				onClick={() => submitSurvey(formValues, history)}
 			>
 				Send Survey
 				<i className='material-icons right'>email</i>
@@ -38,4 +39,4 @@ const mapStateToProps = (state) => ({
 	formValues: state.form.surveyForm.values
 });
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
