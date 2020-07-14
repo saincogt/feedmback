96bf74ba54973c873bc6fecf62c3ac070e4dbf2c
saincogt <saincogt@gmail.com>
2020-07-14 12:05:51 +0930

    prevent back button keeps form data

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -13,4 +14,6 @@ const SurveyNew = () => {
 	return <div>{renderContent()}</div>;
 };
 
-export default SurveyNew;
+export default reduxForm({
+	form: 'surveyForm'
+})(SurveyNew);
