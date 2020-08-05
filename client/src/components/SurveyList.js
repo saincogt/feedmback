dc3d72cf467d025a1ad8078841f5ae21b9fb063f
saincogt <saincogt@gmail.com>
2020-08-05 15:04:53 +0930

    correct some typing

--- client/src/components/SurveyList.js
+++ client/src/components/SurveyList.js
@@ -8,20 +8,20 @@ const SurveyList = ({ surveys, fetchSurveys }) => {
 	}, [fetchSurveys]);
 
 	const renderSurveys = () => {
-		return surveys.reverse().map((survey) => {
+		return surveys.reverse().map((survey, idx) => {
 			return (
-				<div class='card darken-1 blue-grey' key={survey.id}>
-					<div class='card-content white-text'>
-						<span class='card-title'>{survey.title}</span>
+				<div className='card darken-1 blue-grey' key={idx}>
+					<div className='card-content white-text'>
+						<span className='card-title'>{survey.title}</span>
 						<p>{survey.body}</p>
 						<p className='right'>
 							Sent On:{' '}
 							{new Date(survey.dateSent).toLocaleDateString()}
 						</p>
 					</div>
-					<div class='card-action'>
-						<a>Yes: {survey.yes}</a>
-						<a>No: {survey.no}</a>
+					<div className='card-action'>
+						<div>Yes: {survey.yes}</div>
+						<div>No: {survey.no}</div>
 					</div>
 				</div>
 			);
